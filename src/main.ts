56d79b3b5ef30cee251b70c6f5bf5907c3f0efac
tiago de oliveira <deoliv.tiago@proton.me>
2023-07-17 20:42:21 -0300

    chore: init and setup (#2)
    * chore: include prisma
    
    * chore: setup database
    
    * chore: include apollo
    
    * chore: setup graphql
    
    * chore: include class validator
    
    * chore: setup validation
    
    * chore: include coveralls
    
    * build: setup ci
    
    * docs: update readme

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { GraphQLError } from 'graphql';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors) => {
+        const message = errors
+          .map((e) =>
+            Object.values(e.constraints as any)
+              .map((v) => v)
+              .join(', '),
+          )
+          .join(', ');
+
+        return new GraphQLError(`Validation failed: [${message}]`, {
+          extensions: { code: 'BAD_USER_INPUT' },
+        });
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
